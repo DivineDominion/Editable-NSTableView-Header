38625ee72b142fb96f582489235233e237c5f47d
Christian Tietze <christian.tietze@gmail.com>
2016-04-11 10:46:23 +0200

    Initial Commit

---
+++ EditableTableViewHeader/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  EditableTableViewHeader
+//
+//  Created by Christian Tietze on 11/04/16.
+//  Copyright © 2016 Christian Tietze. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
